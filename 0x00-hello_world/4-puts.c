14206bfb0b50fd2c888959f0ee32ffda7e6c69ed
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-06-30 15:37:47 -0700

    strings with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-int main(){
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	char line[] = "Programming is like building a multilingual puzzle";
 	puts(line);
-
 	return 0;
 }
