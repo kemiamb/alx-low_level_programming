0a6d779b0a47ed2ca2b8eabb7980cc8cbfc58187
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-06-30 17:29:33 -0700

    quoting

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 /**
  * main - Entry point
  *
- * Return: 1
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char quote[] ="and  that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and  that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(2, quote, 59);
+	write(2, str1, 59);
 	return (1);
 }
