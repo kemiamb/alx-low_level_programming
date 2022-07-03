e16766d3c4c4d021f58ca6fdf1f1ad04f7e60c98
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-03 04:07:59 -0700

    printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Alphabet printing
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i=0; i < 26; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
