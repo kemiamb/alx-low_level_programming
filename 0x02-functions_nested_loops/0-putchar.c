c05bc9a4783756cccde724732dffc031eabe66f0
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-05 12:36:17 -0700

    printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char line[8] = "_putchar";
+	int i;
+
+
+	for(i = 0; i < 8; i++)
+	{
+		putchar(line[i]);
+	}
+	putchar('\n');
+	return (0);
+}
