2aebd3b221133ebb8aae0b30e69debf4b8d6007d
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-06 14:58:26 -0700

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print-to_98 - prints all natural numbers from n to 98
+ * print_to_98 - prints all natural numbers from n to 98
  * @n: the number to start printing from
  *
  * Return: always 0
