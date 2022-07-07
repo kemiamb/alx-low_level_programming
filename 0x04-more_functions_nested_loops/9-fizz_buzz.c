babab31d9502ec9c36cf47576200e89c17d2ec3a
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-07 15:16:04 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * FizzBuzz - prints for multiple of 3 and 5
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
