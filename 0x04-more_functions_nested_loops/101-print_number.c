e73a12653c399e6975a6d36ff00918d06e8d6aef
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-07 16:32:58 -0700

    try

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an integer
+ * @n: input integer
+ * Return: nothing
+ */
+
+//void print_number(int n)
+int main(void)
+{
+	int i, n;
+	int d = 1;
+	unsigned int x = n;
+	unsigned int y = n;
+	int c = 0;
+
+	if (n == 0)
+	{
+		putchar('0');
+	}
+	if (n < 0)
+	{
+		putchar('-');
+		n = n + 1;
+		n = -n;
+		y = n;
+		x = n;
+		x += 1;
+		y += 1;
+	}
+	while (x != 0)
+	{
+		x = x / 10;
+		c++;
+	}
+	for (i = 1; i < c; i++)
+	{
+		d *= 10;
+	}
+	for (i = 0; i < c; i++)
+	{
+		putchar(y / d + '0');
+		y = y % d;
+		d = d / 10;
+	}
+}
