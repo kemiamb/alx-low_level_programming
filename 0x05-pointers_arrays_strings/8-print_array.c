98ee232ca78facf80372dff0606d28857f0765f5
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-11 17:29:28 -0700

    n elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,16 +3,17 @@
 /**
  * print_array - prints elements of an array of integers
  * @n: number of  element  to be printed
+ * @a: array to be displayed
  * Return: no return
  */
 void print_array(int *a, int n)
 {
 	int  i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (n - 1); i++)
 	{
 		if (i == (n - 1))
-		{ 
+		{
 			printf("%d", *(a + i));
 		}
 		printf("%d, ", *(a + i));
