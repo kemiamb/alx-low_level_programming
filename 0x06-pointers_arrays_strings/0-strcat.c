9c3239ed138e944f88e38ec73a4c7f7ee7466e07
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-13 15:44:45 -0700

    putchar file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @src: source string
+ * @dest: dest string
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	
