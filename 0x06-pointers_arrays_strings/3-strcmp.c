636f21c0aa0c5c0339d27591aac5590bb240aed6
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-14 06:47:48 -0700

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,8 +3,10 @@
  * _strcomp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: 0 if s1 and s2 are equals another number if not
+ * Return: 0 if s1 and s2 are equals 
+ * another number if not
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
@@ -14,9 +16,7 @@ int _strcmp(char *s1, char *s2)
 	while (cmp == 0)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-		{
 			break;
-		}
 		cmp = *(s1 + i) - *(s2 + i);
 		i++;
 	}
