90221b489162e57e77cef79874a6c884b4072360
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-14 03:51:04 -0700

    compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,25 @@
 #include "main.h"
 /**
- * _strcomp - compares twi strings
+ * _strcomp - compares two strings
  * @s1: first string
  * @s2: second string
  * Return: 0 if s1 and s2 are equals another number if not
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, cmp = 0;
+	int i = 0;
+	int cmp = 0;
+
 
 	while (cmp == 0)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+		{
 			break;
+		}
 		cmp = *(s1 + i) - *(s2 + i);
 		i++;
 	}
+
 	return (cmp);
 }
