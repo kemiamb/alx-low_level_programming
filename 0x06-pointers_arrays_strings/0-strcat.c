c0ffe8135699c25c1db9b562795e1b225eb33b50
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-14 03:03:23 -0700

    concanate 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcat - concatenates two strings
+ * @dest: destination string
  * @src: source string
- * @dest: dest string
  * Return: pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
