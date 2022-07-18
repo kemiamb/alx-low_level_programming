30784a431a985e986a38a0b32404da4640c42f1a
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-18 11:40:04 -0700

    sum of arrays

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,10 +13,17 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < (size * size); i++)
 	{
+
 		if (i % (size + 1) == 0)
+		{
 			sum1 += *(a + i);
-		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+		}
+
+		if (i % (size - 1) == 0 && i != 0 && i < (size * size) - 1)
+		{
 			sum2 += *(a + i);
+		}
 	}
+
 	printf("%d\n", sum1, sum2);
 }
