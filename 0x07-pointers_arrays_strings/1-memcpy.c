24440b4bec89bb871ca857789d923f55ba586f93
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-18 10:33:03 -0700

    length of prefix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _memcpy - copies memory area
- * @dest: destination memory area 
- * @src: source memory area 
- * @n: number of bytes
- * Return: pointer to dest
+ * @dest: destination memory area,
+ * @src: source memory area,
+ * @n: bytes filled
+ * Return: the pointer to dest.
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * _strspn - gets the length of a prefix substrig
+ * _strspn - gets the length of a prefix substring
  * @s: initial segment
  * @accept: accepted bytes
- * Return: number of accepted bytes 
+ * Return: the number of accepted bytes
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, bool;
