e954662d9be0da1e4a879677cdd18dbd28ddaa92
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-18 11:28:35 -0700

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array 
+ * Return: nothing to return
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, n = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			n = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - n]);
+	}
+	_putchar('\n');
+}
