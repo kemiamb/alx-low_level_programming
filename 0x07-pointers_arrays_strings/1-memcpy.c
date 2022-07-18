f84fd965b5a321111609a4946d1c70dda044047d
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-18 10:23:04 -0700

    length of prefix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
  * _memcpy - copies memory area
- * @dest: memory area 2
- * @src: memory area 1
+ * @dest: destination memory area 
+ * @src: source memory area 
  * @n: number of bytes
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n){
 	unsigned int i;
 
+
 	for (i = 0; i < n; i++)
+	{
 		*(dest + i) = *(src + i);
+	}
 
 	return (dest);
 }
