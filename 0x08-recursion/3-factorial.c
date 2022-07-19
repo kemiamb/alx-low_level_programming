9193ddf1bf0e4ed6015678fc561367919b94c497
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-19 13:58:17 -0700

    factorial  recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - returns the factoria of a given number
+ * @n: number 
+ * Return: factorial or -1 if n is lower than 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+
+	else
+		return (n * factorial(n - 1));
+}
+
