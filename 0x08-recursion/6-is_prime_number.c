ef7c5df096f396163905b11ecc0536c88d21386a
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-19 15:25:20 -0700

    primer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,26 +5,14 @@
  * @i: iterator
  * Return: 1 if it is prime otherwise return 0
  */
-int prime(int n, int i)
-{
-	if (n % i == 0)
-	{
-		if (n == i)
-			return (1);
-		else
-			return (0);
-	}
-	return (0 + prime(n, i + 1));
-
-
 int is_prime_number(int n)
 {
-	if (n == 0)
+	int i = 2;
+
+
+	if (n % i == 0 && n != i)
 		return (0);
-	if (n < 0)
-		return (0);
-	if (n == 1)
-		return (0);
-	else
-		return (prime(n, 2));
+	if (n % i != 0 && i < n)
+		return (is_prime_number( i + 1, n));
 }
+		
