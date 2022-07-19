a579cede82d481bfa98d7b7b6e9955c6b9c86e2d
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-19 13:50:51 -0700

    fixs

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,11 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s != '\0')
-	{
-		i++;
-		_strlen_recursion(s + 1);
-	}
+	if (*s == '\0')
+		return (0);
+
 	else
-		return (i);
+		return (1 + _strlen_recursion(s + 1));
 }
 
 
