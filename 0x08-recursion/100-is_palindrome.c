435700528e275cdda074cd578f796e16b7a343eb
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-20 16:21:41 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,13 +6,39 @@
  */
 int is_palindrome(char *s)
 {
+	int length;
+
+	length = check(s) - 1;
+	return (pal(s, --length));
+}
+
+/**
+ * check - get the length of a string
+ * @s: string
+ * Return: the length of the string
+ */
+int check(char *s)
+{
 	if (*s == '\0')
 		return (1);
-	
-	if (*s != '\0')
+	else
+		return (1 + check(++s));
+}
+
+/**
+ * pal - recursion of palindrome
+ * @s: string
+ * @l: string's length
+ * Return: 1 if it is a palindrome or  0 if it is not
+ */
+int pal(char *s, int l)
+{
+	if (*s == *(s + 1))
 	{
-		_puts_recursion(s) == _print_rev_recursion(s)
-			return (1);
+		if (l < 0)
+			return (0);
+		else
+			return (pal(++s, l - 2));
 	}
 	else
 		return (0);
