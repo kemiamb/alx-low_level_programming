ecc75a8afff746434f5d0f5d14f9f3e3be5b1a66
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-25 11:33:14 -0700

    printing your command name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * program that prints its name without removing its path before its name
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
