cb44f6d7e1722be44defb644833ea4e7657a5d68
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-25 07:08:45 -0700

    new allocated space in mem

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - returns a pointer to a new string
+ * @str: string
+ * Return: pointer to the duplicated stringor null if insufficient memory
+ */
+char *_strdup(char *str)
+{
+	char *strcpy;
+	unsigned int i, j;
+	
+	
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		strcpy = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strcpy == NULL)
+		return (NULL);
+	for (j = 0; j <= i; j++)
+		strcpy[j] = str[i];
+
+	return (strcpy);
+}
+
+
