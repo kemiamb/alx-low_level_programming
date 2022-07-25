738b80a97ab23e129d677502183fbd022e3a38bf
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-25 13:06:40 -0700

    mul of args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,19 +8,11 @@
  */
 int main (int argc, char *argv[])
 {
-	int mul;
-
-
 	if (argc < 3)
 	{
 		printf("Error\n");
+		return (1);
 	}
-	return (1);
-
-	if (argc >= 3)
-	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", mul);
-	}
+	printf("%d\n", atoi(argv[1] * atoi(argv[2]));
 	return (0);
 }
