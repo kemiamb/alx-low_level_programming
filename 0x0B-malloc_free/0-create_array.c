401fdcb1162292e4c59a09e84131a3be3791e87f
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-25 06:51:50 -0700

    fixed function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,15 +9,20 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *str
+	char *str;
 
 
-	size = malloc(sizeof(c) * size);
 	if (size == 0)
 		return (NULL);
-	if (str == 0)
+
+	str = malloc(sizeof(c) * size);
+
+	if (str == NULL)
 		return (NULL);
+
+
 	for (i = 0; i < size; i++)
 		str[i] = c;
+
 	return (str);
 }	
