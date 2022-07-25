341f938293647c918cb413f2c5df70deef5f5cb5
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-25 11:49:03 -0700

    prints program names

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * program that prints its name without removing its path before its name
+ * main - prints its name without removing its path before its name
  * @argc: arguments
  * @argv: vector of arguments
  * Return: always 0 on success
- */ 
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
