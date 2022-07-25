ecc11e091d360e594d61e5796683b5425ff44970
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-25 07:16:37 -0700

    fixs

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
-		strcpy = (char *)malloc(sizeof(char) * i);
+		strcpy = malloc(sizeof(char) * i);
 
 	if (strcpy == NULL)
 		return (NULL);
