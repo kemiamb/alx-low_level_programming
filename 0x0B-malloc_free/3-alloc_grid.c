07818d945a0bc9a33f38268b5195871a60485b64
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-26 13:22:54 -0700

    two dimension

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,28 +16,15 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = (int **)malloc(height * sizeof(int));
+	ptr = (int *)malloc(height * width * sizeof(int));
 	
-	if (t == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = (int *)malloc(sizeof(int) * width);
 
-		if (ptr[i] == NULL)
-		{
-			for (j = 0; j < i; j++)
-			{
-				free(ptr[j]);
-			}
-			free(ptr);
-		}
 	}
-
-	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-			ptr[i][j] = 0;
 
 	return (ptr);
 }
