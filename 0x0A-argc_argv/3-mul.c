98983e8e69baf0b86417b8f7531efacbc0a0e831
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-07-27 16:07:42 -0700

    mul fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - multiplication of twi arguments
+ * main - multiplication of two arguments
  * @argc: arguments
  * @argv: vector of argument
  * Return: always 0 on success
@@ -11,10 +11,11 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
-       return (0);
+	       	return (0);
 	}
 	else
+	{
 		printf("Error\n");
+	}
 	return (1);
 }
