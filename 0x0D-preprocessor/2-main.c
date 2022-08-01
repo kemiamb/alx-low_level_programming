f18b80f53cff0c4b10c2a7178ada2512d13ad668
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-01 15:13:56 -0700

    print file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from
+ * Return : always 0 on success
+ */
+int main(void)
+{
+	printf("%s\n", __SIZE__);
+	return (0);
+}
