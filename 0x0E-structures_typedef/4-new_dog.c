0f065e2c5982ba13228ae8432c1a1fe055865d65
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-01 18:00:22 -0700

    new dog wahala

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the odg
+ * @owner: owner of the dog
+ * Return: NULL if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, c1, c2;
+	char *n, *o;
+	dog_t *newdog;
+
+	c1 = 0;
+	for (i = 0; name[i] != '\0'; i++)
+		c1++;
+	c2 = 0;
+	for (i = 0; owner != '\0'; i++)
+		c2++;
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
+		return (NULL);
+	n = malloc((c1 + 1) * sizeof(char));
+	if (n == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
+	for (i = 0; i < c1; i++)
+		n[i] = name[i];
+	a[i] = '\0';
+	newdog->name = n;
+	newdog->age = age;
+
+	o = malloc((c2 + 1) * sizeof(char));
+	if (b == NULL)
+	{
+		free(n);
+		free(newdog);
+		return (NULL);
+	}
+	for (i = 0; i < c2; i++)
+		o[i] = owner[i];
+	o[i] = '\0';
+	newdog->owner = o;
+
+	return (newdog);
+}	
