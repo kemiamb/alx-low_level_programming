74a5f09224674184ac1686b3514bf74e2af8e408
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-01 17:06:05 -0700

    init dig

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialize a var of type struct dog
+ * @d: structure name
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ * Return: no return
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
