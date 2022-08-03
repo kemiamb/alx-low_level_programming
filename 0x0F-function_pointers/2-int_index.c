665df40e8255e31a5197563f0811d920cc5e97c5
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-03 13:06:43 -0700

    find an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,15 +5,18 @@
  * int_index - index search
  * @array: array to search
  * @size: size of the array
- * @cmp: function 
+ * @cmp: function
  * Return: index of the first element or -1 if no element match
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+
 	if (size <= 0 || array == NULL || cmp == NULL)
+	{
 		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
