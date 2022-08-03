6912ec0a155aa09b25a5e1a2b9c8c14517a46c65
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-03 12:30:14 -0700

    array changes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array == NULL || action == NULL)
-		exit(98);
+		exit (98);
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
