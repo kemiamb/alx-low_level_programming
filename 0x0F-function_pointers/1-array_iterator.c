d05df5416ae9f94a45510567f32dd409f1adf713
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-03 12:04:01 -0700

    action on arrays

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - execute a function given as a parameter
+ * @array: array 
+ * @size: size of the array
+ * @action: a pointer to the function needed
+ * Return: no return
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL && action == NULL)
+		return (98);
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
