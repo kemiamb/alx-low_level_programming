d5db7fdef40c7a2415251dcbfcc5839da2a12d4e
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-04 05:52:30 -0700

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,25 +7,25 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 4)
+	if (argc == 4)
 	{
-		printf("Error\n");
-		exit(98);
+
+		int a;
+		int b;
+		int (*func)(int, int);
+
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		func = get_op_func(argv[2]);
+		if ((*argv[2] == '%' || *argv[2] == '/') &&b == 0)\
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", func(a, b));
+		return (0);
 	}
-
-	int a;
-	int b;
-	int (*func)(int, int);
-
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	func = get_op_func(argv[2]);
-	if ((*argv[2] == '%' || *argv[2] == '/') &&b == 0)\
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", func(a, b));
-	return (0);
+	printf("Error\n");
+	exit(98);
 }
