9eecb06b42b9c6593f129d3c4fcd4530b8967e38
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-12 23:30:44 +0200

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * start_print - prints a string before the main function
+ * Return: no return
+ */
+void start_print(void)__attibute__((constructor));
+
+void start_print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
