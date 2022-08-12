58d296834c1d736e7ef2f4bd9e53649b38106859
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-12 22:55:57 +0200

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include< stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 /**
- * free_lists - frees a list 
+ * free_list - frees a list
  * @head: head of the list
  * Return: no return
  */
 void free_list(list_t *head)
 {
-	list_t *temporary;
+	list_t *temp;
 
 	while (head != NULL)
 	{
-		temporary = head->next;
+		temp = head->next;
 		free(head->str);
 		free(head);
-		head = temporary;
+		head = temp;
 	}
 }
