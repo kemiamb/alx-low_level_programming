7659cf40ce52f0b8ba4d744598d5187c86a00bdf
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-12 18:07:39 +0200

    count the elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: head of the list
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h =  h->next;
+		i++;
+	}
+
+	return (i);
+}
