65bd90b21997e0012cac7b83bd6935299a6d8779
Kemia Mbongo <kemiambongoiberino@gmail.com>
2022-08-15 10:03:35 +0100

    list of elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_listint - prints all the elements of a list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
